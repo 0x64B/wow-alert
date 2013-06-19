b3224bc34fea6a49aac2dfb18707120790828c98
Thiago Vian <tvian86@gmail.com>
2013-06-19 17:10:41 -0300

    Added callbacks

--- wow-alert.js
+++ wow-alert.js
@@ -1,22 +1,68 @@
     ;
     (function ($) {
-        'use strict';
 
-        window.alert = function (msg, label) {
-            if (!label) {
-                label = "OK"
+
+        var defaults = {
+            label: "OK",
+            autoClose: true
+        };
+
+        jQuery.fn.extend({
+            halfWidth: function () {
+                var width = 0;
+                this.each(function () {
+                    width += $(this).outerWidth() / 2;
+                });
+                return width;
+            },
+            halfHeight: function () {
+                var height = 0;
+                this.each(function () {
+                    height += $(this).outerHeight() / 2;
+                });
+                return height;
             }
-            var content_overlay = $("<div class='wow-alert-overlay'></div><div class='wow-alert-content'><p>" + msg + "</p><a href='#'>" + label + "</a></div>").appendTo("body");
-
-            $('.wow-alert-content').css({
-                left: ($(window).width() - $('.wow-alert-content').outerWidth()) / 2,
-                top: ($(window).height() - $('.wow-alert-content').outerHeight()) / 2
+        });
+        function centerWindow() {
+            this._alertWindow.css({
+                marginLeft: -this._alertWindow.halfWidth(),
+                marginTop: -this._alertWindow.halfHeight()
             });
+        }
 
-            $('.wow-alert-content a').bind('click', function (e) {
+        function createWindow(msg) {
+            var elements = $("<div class='wow-alert-overlay'></div><div class='wow-alert-content'><p>" + msg + "</p><a href='#'>" + this.options.label + "</a></div>");
+            this._alertOverlay = $(elements[0]);
+            this._alertWindow = $(elements[1]);
+
+            this._actionButton = this._alertWindow.find('a');
+
+            this._alertOverlay.appendTo("body");
+            this._alertWindow.appendTo("body");
+
+            return [this._alertOverlay, this._alertWindow];
+        }
+
+        function configureActions() {
+            var context = this;
+
+            this._actionButton.bind('click', function (e) {
                 e.preventDefault();
-                content_overlay.remove();
+                if (context.options.autoClose) close();
+                if (context.options.success) context.options.success();
             });
+        }
+
+        function close() {
+            this._alertOverlay.remove();
+            this._alertWindow.remove();
+        }
+        window.alert = function (msg, opts) {
+            this.options = $.extend(defaults, opts);
+
+            createWindow(msg);
+            centerWindow();
+            configureActions();
 
         }
 
